8b6420aab918c07d1b6fe661932c5dfd35d5efb6
Adrian L Lange <adrianlund@gmail.com>
2009-07-15 01:36:36 +0200

    Initial commit

---
+++ Reviver.lua
@@ -0,0 +1,32 @@
+local corpsetip = string.format('^%s$', string.gsub(CORPSE_TOOLTIP, '%%s', '([^ ]+)'))
+
+local spells = {
+	[GetSpellInfo(20484)] = true, -- Rebirth
+	[GetSpellInfo(29166)] = true, -- Innervate
+}
+
+local function channelName()
+	local _, itype = IsInInstance()
+	if(itype == 'pvp' or itype == 'arena') then
+		return
+	elseif(GetNumRaidMembers() > 0) then
+		return 'RAID'
+	elseif(GetNumPartyMembers() > 0) then
+		return 'PARTY'
+	end
+end
+
+local function onEvent(self, event, unit, spellName, spellRank, unitName)
+	if(unit == 'player' and spells[spellName] and getChannel()) then
+		if(unitName == UNKNOWN) then
+			local _, _, tempName = string.find(GameTooltipTextLeft1:GetText(), corpsetip)
+			unitName = tempName
+		end
+
+		SendChatMessage(string.format('- Casting %s on %s -', spellName, unitName), channelName())
+	end
+end
+
+local addon = CreateFrame('Frame')
+addon:SetScript('OnEvent', onEvent)
+addon:RegisterEvent('UNIT_SPELLCAST_SENT')
